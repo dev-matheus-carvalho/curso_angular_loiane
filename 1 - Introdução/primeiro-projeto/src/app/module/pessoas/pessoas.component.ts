dd6f369564e12ba7d85e053919e59a5a7f49e8bd
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-10-09 15:28:40 -0300

    Criação de templates (Diretivas)

--- 1 - Introdução/primeiro-projeto/src/app/module/pessoas/pessoas.component.ts
+++ 1 - Introdução/primeiro-projeto/src/app/module/pessoas/pessoas.component.ts
@@ -5,6 +5,13 @@ import { Component } from '@angular/core';
   templateUrl: './pessoas.component.html',
   styleUrls: ['./pessoas.component.css']
 })
+
 export class PessoasComponent {
+  personagens: string;
+  arrayPersonagens: Array<string> = ['Nino', 'Pedro', 'Bia', 'Zequinha', 'Morgana', 'Celeste', 'Tio Vitor'];
+
+  constructor() {
+    this.personagens = 'Nino || Pedro || Bia || Zequinha || Morgana || Celeste || Tio Vitor';
+  }
 
 }
