2aec0045e39b2f76a5544b011a34106891c251af
Matheus Carvalho <devmatheuscarvalho@gmail.com>
2023-10-09 15:01:10 -0300

    Criação de modules e componentes

--- 1 - Introdução/primeiro-projeto/src/app/app.module.ts
+++ 1 - Introdução/primeiro-projeto/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { CursosModule } from './cursos/cursos.module';
+import { PessoasModule } from './module/pessoas/pessoas.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { FirstComponentComponent } from './components/first-component/first-comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CursosModule,
+    PessoasModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
